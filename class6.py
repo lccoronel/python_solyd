25bd6819b26fd8b43f4ec98090fbe92ed12d7373
Lucas Coronel <lccoronel7@icloud.com>
2021-03-19 09:39:57 -0300

    finished class 6

---
+++ class6.py
@@ -0,0 +1,14 @@
+nomes = ['Lucas', 'Leonardo', 'Lilian', 'Lucinha']
+
+for nome in nomes:
+  print(nome)
+
+nums_list = range(1, 10)
+
+for num in nums_list:
+  print(num)
+
+i = 0
+while i < 10: 
+  print('valor', i)
+  i += 1 
