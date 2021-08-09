b9bba720011704419091e80d47b7817f1dfe7b43
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-08-09 07:57:19 -0300

    feat: request on weather API [WIP]

--- class-15.py
+++ class-15.py
@@ -1,9 +1,18 @@
 import requests
 import json
 
+# Cotacao
 requisicao = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL') 
 
 cotacao = json.loads(requisicao.text)
 
 print('### COTAÇÃO ###')
 print(cotacao['USDBRL']['bid'])
+
+# Tempo
+cidade = input('Escreva sua cidade: ')
+
+response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=4f0302c8e731a3a878e7033adf44457b')
+
+json_response = json.loads(response.text)
+print(json_response)
